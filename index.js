973e3e294e946ec706f9e693ba954029b15efec0
Tien Do <tiendo220199@gmail.com>
2020-09-02 14:44:43 +0700

    Create Router, Controller folder

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const route = require('./routes');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 // app.engine('handlebars', exphbs());
@@ -17,10 +23,8 @@ app.use(morgan('combined'));
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-app.get('/', (req, res) => {
-  res.render('home');
-  // res.render('home', {layout: false});
-})
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
